0946f585b8316cd03f716fdcb7543c7c85472d04
Prathamesh Mane <prathameshmane3622@gmail.com>
2026-07-15 16:52:13 +0530

    feat: add route protection middleware

---
+++ apps/web/proxy.ts
@@ -0,0 +1,30 @@
+import { getSessionCookie } from "better-auth/cookies";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function proxy(request: NextRequest) {
+    const session = getSessionCookie(request);
+
+    const isAuthPage = request.nextUrl.pathname.startsWith('/auth')
+    const isProtected = 
+        request.nextUrl.pathname.startsWith('/dashboard') ||
+        request.nextUrl.pathname.startsWith('/s') ||
+        request.nextUrl.pathname.startsWith('/r')
+
+    if(!session && isAuthPage) {
+        return NextResponse.next();
+    }
+    
+    if(!session && isProtected) {
+        return NextResponse.redirect(new URL('/auth/signin', request.url))
+    }
+
+    if(session && isAuthPage) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/dashboard/:path*', '/s/:path*', '/r/:path*', '/auth/:path*']
+}
